006e3dd4820228b1a234653abc4415a8ec34e16a
Faidunni <hazeezfaizat@gmail.com>
2025-01-06 20:57:43 +0000

    logic

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,49 @@ function Card() {
   const [amount, setAmount] = useState("");
   const [yearly, setYearly] = useState("");
   const [percentage, setPercentage] = useState("");
+  const [selectedType, setSelectedType] = useState("");
+  const [result, setResult] = useState(null);
+  const [yearResult, setYearResult] = useState(null);
+  const [error, setError] = useState(null);
 
-  const calculation = (amount * yearly * percentage) / 100;
-  const [selectedType, setSelectedType] = useState("repayment");
+  // format total
+  function formatCurrency(total) {
+    return new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: "GBP",
+      minimumFractionDigits: 2,
+    }).format(total);
+  }
+
+  // clear all
+  function handleClear() {
+    setAmount("");
+    setPercentage("");
+    setYearly("");
+    setSelectedType("");
+    setResult(null);
+    setYearResult(null);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!percentage || !amount || !selectedType || !yearly) {
+      setError("This field is required");
+      return;
+    }
+    let calculatedResult = 0;
+    let yearCalculatedResult = 0;
+    if (selectedType === "repayment") {
+      calculatedResult = (amount * yearly * percentage) / 100;
+      yearCalculatedResult = (amount * yearly * percentage * 1000) / 100;
+    } else if (selectedType === "interest") {
+      calculatedResult = (amount + yearly + percentage) / 100;
+      yearCalculatedResult = (amount + yearly + percentage * 1000) / 100;
+    }
+    setResult(calculatedResult);
+    setYearResult(yearCalculatedResult);
+  }
+
   return (
     <main>
       <Calculator
@@ -28,8 +68,16 @@ function Card() {
         onPercentage={setPercentage}
         selectedType={selectedType}
         onSelectedType={setSelectedType}
+        onHandleClear={handleClear}
+        onSubmit={handleSubmit}
+        error={error}
       />
-      <Result />
+      <Result
+        result={result}
+        onClick={handleSubmit}
+        yearResult={yearResult}
+        formatCurrency={formatCurrency}
+      />
     </main>
   );
 }
@@ -43,48 +91,60 @@ function Calculator({
   percentage,
   selectedType,
   onSelectedType,
+  onHandleClear,
+  onSubmit,
+  error,
 }) {
   return (
     <section className="form-area">
       <div className="title">
         <h2>Mortage Calculator</h2>
-        <div>Clear All</div>
+        <div onClick={onHandleClear}>Clear All</div>
       </div>
 
       <form>
         <div className="input-group">
           <label>Mortage Amount</label>
-          <div className="input-with-symbol">
-            <span>£</span>
+          <div className={`input-with-symbol ${!amount ? "input-error" : ""}`}>
+            <span className="error">£</span>
             <input
-              type="text"
+              type="number"
               value={amount}
               onChange={(e) => onChange(Number(e.target.value))}
             />
           </div>
+          {!amount && <small>{error}</small>}
         </div>
-        <div>
-          <div className="input-group">
+        <div className="input-box">
+          <div className="input-field field">
             <label>Mortgage Term</label>
-            <div className="input-with-label">
+            <div className={`input-with-year ${!yearly ? "input-error" : ""}`}>
               <input
-                type="text"
+                type="number"
                 value={yearly}
                 onChange={(e) => onYearly(Number(e.target.value))}
               />
-              <span>years</span>
+
+              <span className="year error">years</span>
             </div>
+            {!yearly && <small>{error}</small>}
           </div>
-          <div className="input-group">
+          <div className="input-field field">
             <label>Interest Rate</label>
-            <div className="input-with-label">
+            <div
+              className={`input-with-percentage ${
+                !percentage ? "input-error" : ""
+              }`}
+            >
               <input
-                type="text"
+                type="number"
                 value={percentage}
                 onChange={(e) => onPercentage(Number(e.target.value))}
               />
-              <span>%</span>
+
+              <span className="percentage error">%</span>
             </div>
+            {!percentage && <small>{error}</small>}
           </div>
         </div>
         <div>
@@ -101,14 +161,16 @@ function Calculator({
           <div className="tip">
             <input
               type="radio"
-              value="repayment"
-              checked={selectedType === "repayment"}
+              value="interest"
+              checked={selectedType === "interest"}
               onChange={(e) => onSelectedType(e.target.value)}
             />
             <span>Interest Only</span>
           </div>
+          {!selectedType && <small>{error}</small>}
         </div>
-        <button>
+
+        <button onClick={onSubmit}>
           <img src={icon} alt="calculator" />
           Calculate Repayments
         </button>
@@ -116,15 +178,37 @@ function Calculator({
     </section>
   );
 }
-function Result() {
+
+function Result({ result, yearResult, formatCurrency }) {
   return (
     <div className="result-area">
-      <img src={empty} alt={empty} />
-      <h2>Results shown here</h2>
-      <p>
-        Complete the form and click “calculate repayments” to see what your
-        monthly repayments would be.
-      </p>
+      {!result && yearResult === null ? (
+        <div className="incompleted">
+          <img src={empty} alt="Empty illustration" />
+          <h2>Results shown here</h2>
+          <p>
+            Complete the form and click “calculate repayments” to see what your
+            monthly repayments would be.
+          </p>
+        </div>
+      ) : (
+        <div className="complete-message">
+          <h2>Your results</h2>
+          <p>
+            Your results are shown below based on the information you provided.
+            To adjust the results, edit the form and click “calculate
+            repayments” again.
+          </p>
+          <div className="result-message">
+            <div>
+              <p>Your monthly repayments</p>
+              <h2>{formatCurrency(result)}</h2>
+            </div>
+            <p>Total you'll repay over the term</p>
+            <h2>{formatCurrency(yearResult)}</h2>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
